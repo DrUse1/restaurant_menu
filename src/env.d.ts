cf07d1fed30508de6ccb8de3226482805e883aa1
Mohamed <opvxgame@gmail.com>
2023-10-13 01:01:29 +0200

    a

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,25 @@
 /// <reference types="astro/client" />
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type TCard = {
+  title: string;
+  sections: TSection[];
+};
+
+type TSection = {
+  title: string;
+  categories?: {
+    title: string;
+    dishes: TDish[];
+  }[];
+  dishes?: TDish[];
+};
+
+type TDish = {
+  title: string;
+  description: string;
+  price: number;
+};
